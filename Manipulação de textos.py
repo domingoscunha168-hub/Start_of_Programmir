8d3aa2f27f0a6bcd28a35dac2e2e818933d29fca
Domingos Cunha <domingoscunha168@gmail.com>
2026-08-20 06:33:46 +0100

    Update phrase to English and modify print statement

--- Manipulação de textos.py
+++ Manipulação de textos.py
@@ -1,3 +1,3 @@
-frase = ('Gratidao Senhor Por Trouxer o Meu Pai De Volta Para Sua Casa')
+frase = ('Thank you Lord for bringing my Father back.')
 print(f"'{'o' in frase}, {'a' in frase}, {'e' in frase}, {'i' in frase}, {'k' in frase}'")
 
